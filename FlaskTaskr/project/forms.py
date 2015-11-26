4086d1ce88b39c06c0c758ef39903ca4ebee1e51
macher <mateuszmejsner@gmail.com>
2015-11-26 20:00:59 +0100

    fixeixes random syntax errors

--- FlaskTaskr/project/forms.py
+++ FlaskTaskr/project/forms.py
@@ -4,16 +4,18 @@ from wtforms.validators import DataRequired
 
 
 class AddTaskForm(Form):
-	task_id = IntegerField()
-	name = StringField('Task Name', validators=[DataRequired()])
-	due_date = DataField(
-		'Date Due (mm/dd/yyyy)',
-		validators = [DataRequired()], format'%m/%d/%Y'
-		)
-	priority = SelectField(
-		'Priority',
-		validators=[DataRequired()],
-		choices=[('1','1'), ('2','2'), ('3','3'), ('4','4'), ('5','5'), 
-				('6','6'), ('7','7'), ('8','8'), ('9','9'), ('10','10'), ]
-		)
-	status = IntegerField('Status')
+    task_id = IntegerField()
+    name = StringField('Task Name', validators=[DataRequired()])
+    due_date = DateField(
+                        'Date Due (mm/dd/yyyy)',
+                        validators=[DataRequired()], format='%m/%d/%Y'
+                        )
+    priority = SelectField(
+        'Priority',
+        validators=[DataRequired()],
+        choices=[
+                ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
+                ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
+                ]
+        )
+    status = IntegerField('Status')
--- FlaskTaskr/project/views.py
+++ FlaskTaskr/project/views.py
@@ -21,6 +21,7 @@ def login_required(test):
             return flask.redirect(flask.url_for('login'))
     return wrap
 
+
 @app.route('/logout/')
 def logout():
     flask.session.pop('logged_in', None)
@@ -39,66 +40,83 @@ def login():
             flask.session['logged_in'] = True
             flask.flash('Welcome!')
             return flask.redirect(flask.url_for('tasks'))
-    flask.render_template('login.html')
+    return flask.render_template('login.html')
+
 
 @app.route('/stats/')
 @login_required
 def tasks():
-	flask.g.db = connect_db()
-	cur = g.db.execute(
-		'SELECT name, due_date, priority, task_id \
-		FROM tasks \
-		WHERE status=1')
-	open_tasks = dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3] for row in cur.fetchall())
-	cur = g.db.execute(
-		'SELECT name, due_date, priority, task_id \
-		FROM tasks \
-		WHERE status=0')
-	closed_tasks = dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3] for row in cur.fetchall())
-	flask.g.db.close()
-	return flask.render_template(
-		'tasks.html'
-		form=AddTaskForm(flask.request.form),
-		open_tasks=open_tasks,
-		closed_tasks=closed_tasks)
+    flask.g.db = connect_db()
+    cur = flask.g.db.execute(
+        'SELECT name, due_date, priority, task_id \
+        FROM tasks \
+        WHERE status=1')
+    open_tasks = [dict(
+        name=row[0], due_date=row[1],
+        priority=row[2], task_id=row[3])
+        for row in cur.fetchall()]
+    cur = flask.g.db.execute(
+        'SELECT name, due_date, priority, task_id \
+        FROM tasks \
+        WHERE status=0')
+    closed_tasks = [dict(
+        name=row[0], due_date=row[1],
+        priority=row[2], task_id=row[3])
+        for row in cur.fetchall()
+    ]
+    flask.g.db.close()
+    return flask.render_template(
+        'tasks.html',
+        form=AddTaskForm(flask.request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks)
 
-@app.route('/add/', methods=['POST',])
+
+@app.route('/add/', methods=['POST', ])
 @login_required
 def new_task():
-	flask.g.db = connect_db()
-	name = flask.request.form['name']
-	date = flask.request.form['due_date']
-	priority = flask.request.form['priority']
-	if not name or not date or not priority:
-		flask.flash("All fields are required. Please try again")
-		return flask.redirect(flask.url_for('tasks'))
-	else:
-		flask.g.db.execute("INSERT INTO tasks (name, due_date, priority, status) \
-							VALUES(?, ?, ?, 1) ", 
-							[name, date, priority])
-		flask.g.db.commit()
-		flask.g.db.close()
-		flask.flash('New entry was successfully posted. Thanks!')
-		return flask.redirect(flask.url_for('tasks'))
+    flask.g.db = connect_db()
+    name = flask.request.form['name']
+    date = flask.request.form['due_date']
+    priority = flask.request.form['priority']
+    if not name or not date or not priority:
+        flask.flash("All fields are required. Please try again")
+        return flask.redirect(flask.url_for('tasks'))
+    else:
+        flask.g.db.execute("INSERT INTO tasks (name, due_date, priority, status) \
+                           VALUES(?, ?, ?, 1) ",
+                           [name, date, priority]
+                           )
+        flask.g.db.commit()
+        flask.g.db.close()
+        flask.flash('New entry was successfully posted. Thanks!')
+        return flask.redirect(flask.url_for('tasks'))
 
 
 @app.route('/complete/<int:task_id>')
 @login_required
 def complete():
-	flask.g.db = connect_db()
-	flask.g.db.execute("UPDATE tasks SET status = 0 WHERE task_id==?",[task_id,])
-	flask.g.db.commit()
-	flask.g.db.close()
-	flask.flash("The task has been marked as complete.")
-	return flask.redirect(flask.url_for("tasks"))
+    flask.g.db = connect_db()
+    flask.g.db.execute("UPDATE tasks \
+                        SET status = 0 \
+                        WHERE task_id==?",
+                        [task_id, ]
+                       )
+    flask.g.db.commit()
+    flask.g.db.close()
+    flask.flash("The task has been marked as complete.")
+    return flask.redirect(flask.url_for("tasks"))
 
 
 @app.route('/delete/<int:task_id>')
 @login_required
 def delete_entry():
-	flask.g.db = connect_db()
-	flask.g.db.execute("DELETE FROM tasks WHERE task_id==?",[task_id,])
-	flask.g.db.commit()
-	flask.g.db.close()
-	flask.flash("The task has been deleted.")
-	return flask.redirect(flask.url_for("tasks"))
+    flask.g.db = connect_db()
+    flask.g.db.execute("DELETE FROM tasks \
+                        WHERE task_id==?",
+                        [task_id, ]
+                       )
+    flask.g.db.commit()
+    flask.g.db.close()
+    flask.flash("The task has been deleted.")
+    return flask.redirect(flask.url_for("tasks"))
